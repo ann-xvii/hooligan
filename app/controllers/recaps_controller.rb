19e1ccc8c4676218495c1151b543aa007a02acba
An.nA <anna.git3@gmail.com>
2015-03-11 04:14:32 -0700

    work on news parser

--- app/controllers/recaps_controller.rb
+++ app/controllers/recaps_controller.rb
@@ -23,7 +23,34 @@ class RecapsController < ApplicationController
 		soccerlens_data.at_css(".articleindex .postWrapper .postTitle a").attr("href")
 
 		# article excerpt
-		soccerlens_data.at_css(".articleindex .postWrapper .article-excerpt").text.strip
+		@posts = soccerlens_data.at_css(".articleindex .postWrapper .article-excerpt").text.strip
+
+
+
+		mechanize = Mechanize.new
+		soccerlens_page = mechanize.get("http://soccerlens.com/")
+		# mechanize.get("http://soccerlens.com/").search(".articleindex")
+		soccerlens_page.link_with(text: "\nRead More →")
+
+
+
+		### HIERARCHY
+		### .articleindex .postWrapper
+
+		# postTitle comes first, # the title is a link
+		@ludicrous_title = mechanize.page.parser.at_css(".articleindex .postWrapper .postTitle").text
+		@ludicrous_link = mechanize.page.parser.at_css('.articleindex .postWrapper .postTitle a').attr("href")
+
+		# get article date and author
+		@ludicrous_date = mechanize.page.parser.at_css(".articleindex .postWrapper .articlemeta").text
+		@ludicrous_author_link = mechanize.page.parser.css(".articleindex .postWrapper .articlemeta a")[0].attr("href")
+
+		# get article excerpt, get image
+		@ludicrous_display = mechanize.page.parser.at_css(".articleindex .postWrapper .article-excerpt p").text.strip
+		@ludicrous_image = mechanize.page.parser.at_css(".articleindex .postWrapper .article-excerpt .featured-image img").attr("src")
+
+		# @full_article_link = soccerlens_page.link_with(text: "Read More")
+		# soccerlens_page.link_with(href: @ludicrous_link)
 	end
 
 end
