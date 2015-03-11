9523bcd1e70201cb65654b25f4f6e8c33e80ef80
An.nA <anna.git3@gmail.com>
2015-03-11 07:04:12 -0700

    modify scraper, add style

--- app/controllers/recaps_controller.rb
+++ app/controllers/recaps_controller.rb
@@ -30,9 +30,7 @@ class RecapsController < ApplicationController
 		mechanize = Mechanize.new
 		soccerlens_page = mechanize.get("http://soccerlens.com/")
 		# mechanize.get("http://soccerlens.com/").search(".articleindex")
-		soccerlens_page.link_with(text: "\nRead More →")
-
-
+		# soccerlens_page.link_with(text: "\nRead More →")
 
 		### HIERARCHY
 		### .articleindex .postWrapper
@@ -40,17 +38,27 @@ class RecapsController < ApplicationController
 		# postTitle comes first, # the title is a link
 		@ludicrous_title = mechanize.page.parser.at_css(".articleindex .postWrapper .postTitle").text
 		@ludicrous_link = mechanize.page.parser.at_css('.articleindex .postWrapper .postTitle a').attr("href")
-
+		link = mechanize.page.parser.at_css('.articleindex .postWrapper .postTitle a').attr("href")
 		# get article date and author
 		@ludicrous_date = mechanize.page.parser.at_css(".articleindex .postWrapper .articlemeta").text
 		@ludicrous_author_link = mechanize.page.parser.css(".articleindex .postWrapper .articlemeta a")[0].attr("href")
 
 		# get article excerpt, get image
-		@ludicrous_display = mechanize.page.parser.at_css(".articleindex .postWrapper .article-excerpt p").text.strip
+		@ludicrous_excerpt = mechanize.page.parser.at_css(".articleindex .postWrapper .article-excerpt p").text.strip
 		@ludicrous_image = mechanize.page.parser.at_css(".articleindex .postWrapper .article-excerpt .featured-image img").attr("src")
 
 		# @full_article_link = soccerlens_page.link_with(text: "Read More")
-		# soccerlens_page.link_with(href: @ludicrous_link)
+		# new_link = soccerlens_page.link_with(href: link)
+		# soccerlens_page = new_link.click
+		# @full_text = mechanize.page.parser.css("#single-content p").text.strip
+		articles_container = Array.new
+
+		articles_container 
+
+		mechanize.page.parser.css(".articleindex").each do |article|
+			text = article.at_css(".postWrapper .postTitle").text
+			puts text
+		end
 	end
 
 end
